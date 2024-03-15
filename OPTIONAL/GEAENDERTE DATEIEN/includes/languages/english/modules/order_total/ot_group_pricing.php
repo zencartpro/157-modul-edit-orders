b2e0083d003889f9643b08f0877421310e5e1221
Jörg Schönberger <office@webchills.at>
2024-03-15 15:25:44 +0100

    optional core edits for ot_coupon and ot_group_pricing

---
+++ OPTIONAL/GEAENDERTE DATEIEN/includes/languages/english/modules/order_total/ot_group_pricing.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* modified language file for Edit Orders 4.7.1
+* @copyright Copyright 2003-2024 Zen Cart Development Team
+* Zen Cart German Version - www.zen-cart-pro.at
+* @copyright Portions Copyright 2003 osCommerce
+* @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
+* @version $Id: ot_group_pricing.php for Edit Orders 2024-03-15 15:49:16Z webchills $
+*/
+
+define('MODULE_ORDER_TOTAL_GROUP_PRICING_TITLE', 'Group Discount');
+define('MODULE_ORDER_TOTAL_GROUP_PRICING_DESCRIPTION', 'Group Discount');
+define('MODULE_ORDER_TOTAL_GROUP_PRICING_SUBTOTAL_TEXT',', sub-total');
+define('MODULE_ORDER_TOTAL_GROUP_PRICING_SHIPPING_TEXT',' and shipping');    //- Note: Leading space needed!
+define('MODULE_ORDER_TOTAL_GROUP_PRICING_TAX_REDUCED_TEXT',', tax reduced');
+define('MODULE_ORDER_TOTAL_GROUP_PRICING_INCL_TEXT',' incl');               //- Note: Leading space needed!
