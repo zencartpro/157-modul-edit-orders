fec27fb73018704a9b91ecf867368bd69c09c178
Jörg Schönberger <office@webchills.at>
2026-02-16 17:00:31 +0100

    restructured for release

--- NEUE DATEIEN/zc_plugins/EditOrders/v5.0.3/catalog/includes/languages/english/modules/order_total/ot_misc_cost.php
+++
@@ -1,6 +0,0 @@
-<?php
-// -----
-// Gather any Miscellaneous Cost for an order, an order-total created by lat9 (https://vinosdefrutastropicales.com)
-//
-define('MODULE_ORDER_TOTAL_MISC_COST_TITLE', 'Misc. Cost');
-define('MODULE_ORDER_TOTAL_MISC_COST_DESCRIPTION', 'A miscellaneous cost associated with the order.');
--- NEUE DATEIEN/zc_plugins/EditOrders/v5.0.3/catalog/includes/languages/german/modules/order_total/ot_misc_cost.php
+++
@@ -1,6 +0,0 @@
-<?php
-// -----
-// Gather any Miscellaneous Cost for an order, an order-total created by lat9 (https://vinosdefrutastropicales.com)
-//
-define('MODULE_ORDER_TOTAL_MISC_COST_TITLE', 'Diverse Kosten');
-define('MODULE_ORDER_TOTAL_MISC_COST_DESCRIPTION', 'Verschiedene mögliche Kosten in Verbindung mit einer Bestellung.');
